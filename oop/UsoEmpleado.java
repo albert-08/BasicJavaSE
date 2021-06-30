9643ec97629573e958890a7b6546f3dce9e466d1
alberto <alberto.floreso94@gmail.com>
2021-06-30 16:27:12 -0500

    add new class

---
+++ oop/UsoEmpleado.java
@@ -0,0 +1,20 @@
+package oop;
+
+import java.util.Date;
+
+public class UsoEmpleado {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Empleado {
+
+    private String nombre;
+    private double sueldo;
+    private Date altaContrato;
+
+    public Empleado(String nom, double sue, int agno, int mes, int dia) {
+        
+    }
+}
